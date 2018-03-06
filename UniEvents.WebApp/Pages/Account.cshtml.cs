e93afab410af3b9421ee94057b810c7260fc9256
Nathan <Nathan.J.Ullman@gmail.com>
2018-03-06 16:47:47 -0600

    Add more tabs to layout, also started with navigation links between pages, learned to commit total project instead of one piece

---
+++ UniEvents.WebApp/Pages/Account.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UniEvents.WebApp.Pages
+{
+    public class AccountModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+    }
+}
---
+++ UniEvents.WebApp/Pages/Login.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UniEvents.WebApp.Pages
+{
+    public class SignUpModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
