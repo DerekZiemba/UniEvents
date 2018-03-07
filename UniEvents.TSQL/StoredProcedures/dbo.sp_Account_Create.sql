420047287f8d77c05461539d41dcb1a829c04537
DerekZiemba <DerekZiemba@gmail.com>
2018-03-06 22:51:26 -0600

    dbo.sp_Account_Create

--- UniEvents.TSQL/StoredProcedures/dbo.sp_Account_Create.sql
+++ UniEvents.TSQL/StoredProcedures/dbo.sp_Account_Create.sql
@@ -6,29 +6,57 @@ GO
 -- Author:		<Derek Ziemba>
 -- Create date: <2-16-2018>
 -- =============================================
-USE [dbUniHangouts]
+USE [$(dbUniHangouts)]
 GO
 CREATE OR ALTER PROCEDURE [dbo].[sp_Account_Create]
-	@UserName varchar(20),
-	@Password nvarchar(40),
-	@DisplayName nvarchar(50) = NULL,		
-	@FirstName nvarchar(20) = NULL,
-	@LastName nvarchar(20) = NULL,
-	@ContactEmail varchar(50) = NULL,
-	@PhoneNumber varchar(20) = NULL,
-	@Description nvarchar(4000) = NULL,
-	@LocationID bigint = NULL,
-	@IsGroup bit = 0,
-	@GroupOwnerAccountID bigint = NULL
+	@UserName VARCHAR(20),
+	@Password NVARCHAR(50) = NULL,
+	@PasswordHash BINARY(256) = NULL,
+	@LocationID BIGINT,
+	@DisplayName NVARCHAR(50) = NULL,		
+	@FirstName NVARCHAR(20) = NULL,
+	@LastName NVARCHAR(20) = NULL,
+	@ContactEmail VARCHAR(50) = NULL,
+	@PhoneNumber VARCHAR(20) = NULL,
+	@Description NVARCHAR(4000) = NULL,	
+	@GroupOwnerAccountID BIGINT = NULL
 AS
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 SET NOCOUNT ON;
 
-IF (@IsGroup = 1)
+IF EXISTS (SELECT TOP 1 * FROM [dbo].Accounts WHERE UserName = @UserName) RAISERROR('UserName_Taken', 11, 1);
+
+IF NOT EXISTS (SELECT  TOP 1 * FROM [dbo].Locations WHERE LocationID = @LocationID) RAISERROR('Location_Invalid', 12, 2);
+
+IF @GroupOwnerAccountID > 0
 	BEGIN
-		
+		IF NOT EXISTS (SELECT TOP 1 * FROM [dbo].Accounts WHERE AccountID = @GroupOwnerAccountID) RAISERROR('GroupOwner_Invalid', 13, 3);
+
+		INSERT INTO dbo.Accounts (UserName, DisplayName, PasswordHash, IsGroup) VALUES (@UserName, @DisplayName, @PasswordHash, 1);
+		DECLARE @GroupID BIGINT = SCOPE_IDENTITY();
+
+		INSERT INTO [dbo].AccountDetails(AccountID, LocationID, FirstName, LastName, ContactEmail, PhoneNumber, [Description]) 
+			VALUES (@GroupID, @LocationID, @FirstName, @LastName, @ContactEmail, @PhoneNumber, @Description);
+
+		INSERT INTO dbo.MapAccountsAndGroups(AccountID, GroupID, IsGroupOwner, IsGroupAdmin, IsGroupFriend, IsPendingFriend, IsGroupFollower) VALUES 
+			(@GroupOwnerAccountID, @GroupID, 1, 0, 0, 0, 0);
+
+		RETURN @GroupID;
+
 	END
 ELSE
 	BEGIN
+		IF @Password IS NOT NULL AND @PasswordHash IS NULL SET @PasswordHash = HASHBYTES('SHA2_256', @Password + '.nevents');
+		IF @PasswordHash IS NULL RAISERROR('Password_Invalid', 11, 4);
+
+		INSERT INTO dbo.Accounts (UserName, DisplayName, PasswordHash, IsGroup) VALUES (@UserName, @DisplayName, @PasswordHash, 0);
+		DECLARE @AccountID bigint = SCOPE_IDENTITY();
+
+		INSERT INTO [dbo].AccountDetails(AccountID, LocationID, FirstName, LastName, ContactEmail, PhoneNumber, [Description]) 
+			VALUES (@AccountID, @LocationID, @FirstName, @LastName, @ContactEmail, @PhoneNumber, @Description);
+
+		RETURN @AccountID;
 	END
+
+GO
