70038c72b7d316e1beeb92b619af35c0bfdbed22
Nathan <Nathan.J.Ullman@gmail.com>
2018-03-11 11:21:31 -0500

    Added event feed functionality to homepage with some place holder values.Functionality includes:event feed display, click event for modal that would have extra info, join button, 10 more button which adds another event to feed.

--- UniEvents.WebApp/Pages/Home.cshtml.cs
+++ UniEvents.WebApp/Pages/Home.cshtml.cs
@@ -9,9 +9,12 @@ namespace UniEvents.WebApp.Pages
 {
     public class HomePageModel : PageModel
     {
+        public int[] EventFeed = { 1,2,3,4,5,6,7,8,9,10};  // we can use something else instead of array to store data
+
         public void OnGet()
         {
-
+            // to get ten, and then another ten.... do we pull all at once and partiton, or keep track of where we are and pull individual chucks
+            //TODO: pull all events only add ten
         }
     }
 }
