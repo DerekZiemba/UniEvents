a263c3093fcac367e1b9f86ebad9004ec137164c
DerekZiemba <DerekZiemba@gmail.com>
2018-03-12 17:44:12 -0500

    Login Stored Procedures

--- UniEvents.TSQL/StoredProcedures/dbo.sp_Account_Login.sql
+++ UniEvents.TSQL/StoredProcedures/dbo.sp_Account_Login.sql
@@ -5,20 +5,14 @@ GO
 USE [$(dbUniHangouts)]
 GO
 CREATE OR ALTER PROCEDURE [dbo].[sp_Account_Login]
+	@AccountID BIGINT,
 	@UserName VARCHAR(20),
-	@Password NVARCHAR(50) = NULL,
-	@PasswordHash BINARY(256) = NULL,
-	
-	@AccountID BIGINT OUTPUT,
-	@DisplayName NVARCHAR(50) OUTPUT
+	@APIKey BINARY(256),
+	@LoginDate DATETIME
 AS
 SET NOCOUNT ON;
 
-IF @Password IS NOT NULL AND @PasswordHash IS NULL SET @PasswordHash = HASHBYTES('SHA2_256', @Password + '.nevents');
-IF @PasswordHash IS NULL RAISERROR('Password_Invalid', 11, 4);
 
-SELECT TOP 1 @DisplayName = [DisplayName], @AccountID = [AccountID]
-	FROM dbo.Accounts AS acct 
-	WHERE acct.UserName = @UserName AND acct.PasswordHash = @PasswordHash;
-
+INSERT INTO dbo.Logins (AccountID, UserName, APIKey, LoginDate)
+	VALUES (@AccountID, @UserName, @APIKey, @LoginDate);
 GO
