42849c18dff25069cf5f324876a7429f7894263e
DerekZiemba <DerekZiemba@gmail.com>
2018-04-05 16:00:27 -0500

    Fixed Warnings

--- Code/UniEvents.WebApp/GlobalSuppressions.cs
+++ Code/UniEvents.WebApp/GlobalSuppressions.cs
@@ -3,10 +3,6 @@
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
-
-//Because this can make methods too horizontally long to fit on one screen.
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0022:Use expression body for methods", Justification = "<Pending>")]
-//Because this can make object initialization harder to debug.
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "<Pending>")]
-//Because not everyone is using C#7.2
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0034:Simplify 'default' expression", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0022:Use expression body for methods", Justification = "Can make methods too large horizontally to fit on one screen.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "Makes object initialization harder to debug.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0034:Simplify 'default' expression", Justification = "Not everyone is using C#7.2")]
