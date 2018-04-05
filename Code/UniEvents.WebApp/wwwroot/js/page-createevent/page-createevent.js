69e5dd2b07190bc5c2ba3f4e8de744926d4d9ec1
DerekZiemba <DerekZiemba@gmail.com>
2018-04-04 22:04:06 -0500

    CreateEvent page: Added location autocomplete and DatePicker

---
+++ Code/UniEvents.WebApp/wwwroot/js/page-createevent/page-createevent.js
@@ -0,0 +1,52 @@
+﻿
+
+(function () {
+
+   var InputParams = document.getElementById("InputParams");
+   var EventCreationLabel = document.getElementById("EventCreationLabel");
+   var EventCreationButton = document.getElementById("EventCreationButton");
+
+   EventCreationButton.addEventListener("click", function () {
+      var oRequest = U.buildAjaxRequestFromInputs(InputParams.querySelectorAll("input[param]"), { type: "POST", url: "webapi/TODO " });
+
+      function handleFailure(ev) {
+         console.log(oRequest, ev);
+         EventCreationLabel.value = "Failed!";
+         InputParams.class = "Failed";
+         U.setPageMessage('error', ev.message);
+         U.highlightRequiredInputs(true);
+      }
+
+      $.ajax(oRequest)
+         .fail(handleFailure)
+         .done(function (ev) {
+            if (ev.success) {
+               signUpButton.disabled = true;
+               console.log(oRequest, ev);
+               signUpLabel.value = "Success!";
+               InputParams.class = "Success";
+               U.setPageMessage('success', 'Success!  Go to the <a href="/Index">Event feed to see events </a>');
+            } else {
+               handleFailure(ev);
+            }
+         });
+
+   });
+
+   U._locationAutoComplete = new U.LocationAutoComplete({ search: "#searchLocations" });
+
+   var datepickerOptions = {
+      minDate: "today",
+      enableTime: true,
+      inline: true,
+      onReady: function (arg0, arg1, inst) {
+         console.log(inst):
+         inst.showTimeInput = true;
+      }
+   }
+   U._startDatePicker = flatpickr('[param="DateStart"]', datepickerOptions);
+   U._endDatePicker = flatpickr('[param="DateEnd"]', datepickerOptions);
+
+
+}());
+
