528ba4eee4a6ff388db3812cca79f7a7679e8da9
Nathan <Nathan.J.Ullman@gmail.com>
2018-04-11 16:49:31 -0500

    we can send an email!

--- Code/UniEvents.WebApp/Pages/SignUp.cshtml.cs
+++ Code/UniEvents.WebApp/Pages/SignUp.cshtml.cs
@@ -5,13 +5,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.Net.Mail;
 
 namespace UniEvents.WebApp.Pages {
    public class LoginModel : WebAppPageModel {
 
 
       public void OnGet() {
-      }
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.EnableSsl = true;
+            smtp.Port = 587;
+            smtp.Credentials = new NetworkCredential("UniEventsEmail@gmail.com",
+               "gbocwdvvqkvpwwiy");
+            smtp.Send("Nathan.j.ullman@gmail.com", "ullmanator12@gmail.com",
+               "ya boi", "Email message");
+
+        }
 
       public LoginModel(IHttpContextAccessor accessor): base(accessor) { }
    }
