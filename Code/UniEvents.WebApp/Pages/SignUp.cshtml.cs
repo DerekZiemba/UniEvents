3b00679a819940f093f66f5dc0091489adaa6d2d
Nathan <Nathan.J.Ullman@gmail.com>
2018-04-13 16:49:09 -0500

    push button to send email functionality

--- Code/UniEvents.WebApp/Pages/SignUp.cshtml.cs
+++ Code/UniEvents.WebApp/Pages/SignUp.cshtml.cs
@@ -11,16 +11,20 @@ using System.Net.Mail;
 namespace UniEvents.WebApp.Pages {
    public class LoginModel : WebAppPageModel {
 
-
-      public void OnGet() {
+        public  int VerNumber = 1234;
+        public  void SendEmail() 
+        {
+            int randomNumber = 1234;
+            VerNumber = randomNumber;
             SmtpClient smtp = new SmtpClient("smtp.gmail.com");
             smtp.EnableSsl = true;
             smtp.Port = 587;
             smtp.Credentials = new NetworkCredential("UniEventsEmail@gmail.com",
                "gbocwdvvqkvpwwiy");
             smtp.Send("Nathan.j.ullman@gmail.com", "ullmanator12@gmail.com",
-               "ya boi", "Email message");
-
+               "ya boi", VerNumber.ToString());
+        }
+      public void OnGet() {
         }
 
       public LoginModel(IHttpContextAccessor accessor): base(accessor) { }
